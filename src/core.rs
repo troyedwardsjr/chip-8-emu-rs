7e97f446af594334ad3d4388d518f6647f880991
troyedwardsjr <troyedwardsjr@gmail.com>
2022-01-15 01:15:03 -0500

    first commit

---
+++ src/core.rs
@@ -0,0 +1,108 @@
+#![allow(dead_code)]
+pub const SCREEN_WIDTH: usize = 64;
+pub const SCREEN_HEIGHT: usize = 32;
+
+const RAM_SIZE: usize = 4096;
+const STACK_SIZE: usize = 16;
+const NUM_REGS: usize = 16;
+const NUM_KEYS: usize = 16;
+const START_ADDR: u16 = 0x200;
+
+const FONTSET_SIZE: usize = 80;
+const FONTSET: [u8; FONTSET_SIZE] = [
+  0xF0, 0x90, 0x90, 0x90, 0xF0, // 0 
+  0x20, 0x60, 0x20, 0x20, 0x70, // 1 
+  0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2 
+  0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3 
+  0x90, 0x90, 0xF0, 0x10, 0x10, // 4 
+  0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5 
+  0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6 
+  0xF0, 0x10, 0x20, 0x40, 0x40, // 7 
+  0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8 
+  0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9 
+  0xF0, 0x90, 0xF0, 0x90, 0x90, // A 
+  0xE0, 0x90, 0xE0, 0x90, 0xE0, // B 
+  0xF0, 0x80, 0x80, 0x80, 0xF0, // C 
+  0xE0, 0x90, 0x90, 0x90, 0xE0, // D 
+  0xF0, 0x80, 0xF0, 0x80, 0xF0, // E 
+  0xF0, 0x80, 0xF0, 0x80, 0x80 // F
+];
+
+pub struct Emu {
+  screen: [bool; SCREEN_WIDTH * SCREEN_HEIGHT], // Screen
+  pc: u16, // Program Counter - Special register: Index of the current instruction
+  ram: [u8; RAM_SIZE], // RAM
+  v_reg: [u8; NUM_REGS], // V registers,
+  i_reg: u16, // I register - RAM index for reads and writes,
+  sp: u16, // Stack pointer - top of the stack
+  stack: [u16; STACK_SIZE], // Stack
+  keys: [bool; NUM_KEYS], // Keys
+  dt: u8, // Delay timer
+  st: u8 // Sound timer
+}
+
+impl Emu {
+  pub fn new() -> Self {
+    let mut new_emu = Self {
+      pc: START_ADDR,
+      ram: [0; RAM_SIZE],
+      screen: [false; SCREEN_WIDTH * SCREEN_HEIGHT],
+      v_reg: [0; NUM_REGS],
+      i_reg: 0,
+      sp: 0,
+      stack: [0; STACK_SIZE],
+      keys: [false; NUM_KEYS],
+      dt: 0,
+      st: 0,
+    };
+
+    new_emu.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
+
+    new_emu
+  }
+
+  fn push(&mut self, val: u16) {
+    self.stack[self.sp as usize] = val;
+    self.sp += 1;
+  }
+
+  fn pop(&mut self) -> u16 {
+    self.sp -= 1;
+    self.stack[self.sp as usize]
+  }
+
+  pub fn reset(&mut self) {
+    self.pc = START_ADDR;
+    self.ram = [0; RAM_SIZE];
+    self.screen = [false; SCREEN_WIDTH * SCREEN_HEIGHT];
+    self.v_reg = [0; NUM_REGS];
+    self.i_reg = 0;
+    self.sp = 0;
+    self.stack = [0; STACK_SIZE];
+    self.keys = [false; NUM_KEYS];
+    self.dt = 0;
+    self.st = 0;
+    self.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
+  }
+
+  fn fetch(&mut self) -> u16 {
+    let higher_byte = self.ram[self.pc as usize] as u16;
+    let lower_byte = self.ram[(self.pc + 1) as usize] as u16;
+    let op = (higher_byte << 8) | lower_byte;
+    self.pc += 2;
+    op
+  }
+
+  pub fn tick_timers(&mut self) {
+    if self.dt > 0 {
+      self.dt -= 1;
+    }
+
+    if self.st > 0 {
+      if self.st == 1 {
+        // BEEP
+      }
+      self.st -= 1;
+    }
+  }
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod core;
+
+// #[cfg(not(target_arch = "wasm32"))]
+fn main() {
+  let mut emu = core::Emu::new();
+}
